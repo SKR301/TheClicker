2d8a5d7e1ae523f51281833cf5a958c8e3f1de91
skr <skrint301@gmail.com>
2024-10-14 23:28:46 +0530

    Tried with random colors, massive failure

--- app/src/main/java/com/skrinternationals/theclicker/MainActivity.java
+++ app/src/main/java/com/skrinternationals/theclicker/MainActivity.java
@@ -8,13 +8,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout main_layout;
     TextView textView_clicks_counter;
 
-    int COLOR_INCREMENT_STEP = 150;
+    int COLOR_INCREMENT_STEP = 1;
     int COLOR_MAX_VALUE = 16777215;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +28,33 @@ public class MainActivity extends AppCompatActivity {
 
         main_layout = findViewById(R.id.main_layout);
         textView_clicks_counter = findViewById(R.id.textView_clicks_counter);
+        random = new Random();
 
         main_layout.setOnClickListener(view -> {
             int counter = Integer.parseInt(textView_clicks_counter.getText().toString());
             textView_clicks_counter.setText(String.valueOf(counter + COLOR_INCREMENT_STEP));
-            setLayoutWithIntColorVal(main_layout, COLOR_MAX_VALUE - counter);
+            int color_value = random.nextInt(COLOR_MAX_VALUE);
+            setLayoutColorWithIntVal(main_layout, color_value);
+            setTextColorWithIntVal(textView_clicks_counter, ((COLOR_MAX_VALUE - color_value) % COLOR_MAX_VALUE + COLOR_MAX_VALUE) % COLOR_MAX_VALUE);
         });
+
     }
 
-    void setLayoutWithIntColorVal(ConstraintLayout layout, int color_value) {
-
+    void setLayoutColorWithIntVal(ConstraintLayout layout, int color_value) {
         StringBuilder color_value_string = new StringBuilder(Integer.toString(color_value, 16));
         while (color_value_string.length() < 6) {
             color_value_string.insert(0, '0');
         }
-//        Log.d("TAG!!!: ", color_value_string.toString());
+        Log.d("TAG!!!: ", "layout_color: "+color_value);
         layout.setBackgroundColor(Color.parseColor("#"+color_value_string));
+    }
+
+    void setTextColorWithIntVal(TextView text_view, int color_value) {
+        StringBuilder color_value_string = new StringBuilder(Integer.toString(color_value, 16));
+        while (color_value_string.length() < 6) {
+            color_value_string.insert(0, '0');
+        }
+        Log.d("TAG!!!: ", "text_color: "+color_value);
+        text_view.setTextColor(Color.parseColor("#"+color_value_string));
     }
 }
